bfd1fc3e98fd55e2c216cb7b4b2e0822ab43cea4
shritija sawant <sawantshritija15@gmail.com>
2024-09-21 13:39:55 +0530

    2nd update [Runs on the web]

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart'; // Add this import for Firebase Auth
 import 'package:firebase_core/firebase_core.dart';
@@ -5,9 +6,23 @@ import 'package:menu_vista/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  if(kIsWeb) {
+      await Firebase.initializeApp(
+      options: const FirebaseOptions
+        ( apiKey: "AIzaSyBFwOrZUiMrCXz6LWQ0wEfmlt8_qWTgtks",
+          appId: "1:722133009908:web:a1f557a260aa0927f5e5a1",
+          messagingSenderId: "722133009908",
+          projectId: "menuvista-cebae",
+          authDomain: "menuvista-cebae.firebaseapp.com",
+          storageBucket: "menuvista-cebae.appspot.com",
+      )
+    );
+  }
+  else {
+      await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
   runApp(MenuVistaApp());
 }
 
